42c8ab9c1d980d6765bd424e26a5a2cea2233e4c
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2023-11-23 23:16:38 +0100

    a

--- routes/cityRoutes.js
+++ routes/cityRoutes.js
@@ -4,6 +4,13 @@ const cityControllers = require("../controllers/cityControllers")
 const router = express.Router();
 
 router.get("/cities", cityControllers.cityList)
+router.get("/cities1", cityControllers.cityList)
+
+router.get("/cities2", cityControllers.cityList)
+
+router.get("/cities3", cityControllers.cityList)
+router.get("/cities4", cityControllers.cityList)
+router.get("/cities5", cityControllers.cityList)
 
 
 module.exports = router;
